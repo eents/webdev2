e6bbae48181b0ca1752d9298e2e71bdf2a36cd83
eents <60370827+eents@users.noreply.github.com>
2022-03-25 00:16:59 -0500

    A04 finish

--- projects/midterm/app.js
+++ projects/midterm/app.js
@@ -17,11 +17,17 @@ $('#close-mobile-menu-button').click(function() {
 //         $('#mobile-menu').css('display', 'none')
 // }
 
-// $(window).resize(function() {
-//     var sWidth = $(window).width("em")
-//     if (sWidth > 60) {
-//         $('#mobile-menu-button').css('display', 'none')
-//         $('#close-mobile-menu-button').css('display', 'none')
-//         $('#mobile-menu').css('display', 'none')
-//     }
-// })
+let rootFontSize = +$('html').css('font-size').slice(0, 2);
+console.log(rootFontSize)
+
+let pRatio = 16/rootFontSize;
+
+$(window).resize(function() {
+    var sWidth = $(window).width()
+    console.log(sWidth)
+    if (sWidth > 800 * pRatio) {
+        $('#mobile-menu-button').css('display', 'none')
+        $('#close-mobile-menu-button').css('display', 'none')
+        $('#mobile-menu').css('display', 'none')
+    }
+})
